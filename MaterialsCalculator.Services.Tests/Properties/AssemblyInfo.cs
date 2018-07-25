c6aa4465c3e0e8bcb0b019bb2bf5bfacb3c49830
Chris Neale <chris.neale@parexel.com>
2018-07-25 15:57:41 +0100

    US001: add Services projects

---
+++ MaterialsCalculator.Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MaterialsCalculator.Services.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PAREXEL")]
+[assembly: AssemblyProduct("MaterialsCalculator.Services.Tests")]
+[assembly: AssemblyCopyright("Copyright © PAREXEL 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dec4be83-d017-433c-b843-c4350dc99d08")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
