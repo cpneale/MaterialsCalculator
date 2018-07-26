229eb85edcd23cb8dc47b9768a9a05b73252f5b2
Chris Neale <chris.neale@parexel.com>
2018-07-26 14:04:29 +0100

    US001: configure StructureMap

--- MaterialsCalculator.Api/Global.asax.cs
+++ MaterialsCalculator.Api/Global.asax.cs
@@ -6,6 +6,10 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MaterialsCalculator.Api.StructureMap;
+using StructureMap;
+using StructureMap.Graph;
+using WebApi.StructureMap;
 
 namespace MaterialsCalculator.Api
 {
@@ -14,7 +18,10 @@ namespace MaterialsCalculator.Api
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.UseStructureMap(new MaterialsApiRegistry());
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
---
+++ MaterialsCalculator.Api/StructureMap/MaterialsApiRegistry.cs
@@ -0,0 +1,20 @@
+﻿using MaterialsCalculator.Core.Services;
+using MaterialsCalculator.Interfaces.Services;
+using StructureMap;
+
+namespace MaterialsCalculator.Api.StructureMap
+{
+    public class MaterialsApiRegistry : Registry
+    {
+        public MaterialsApiRegistry()
+        {
+            CreateMappings();
+        }
+
+        private void CreateMappings()
+        {
+            For<IPaintService>()
+                .Use<PaintService>();
+        }
+    }
+}
